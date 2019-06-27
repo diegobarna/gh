3680d5610e537f46fcf1163a6f6943a42e8d4bd4
Diego Castañeda <diegobarna@gmail.com>
2019-06-27 10:50:17 +0200

    Remove unnecessary comment

--- gh.sh
+++ gh.sh
@@ -1,21 +1,17 @@
 #!/bin/sh
-#
-# Opens the github page for a repo/branch in your browser.
-#
-# gh [remote] [branch]
 
 git rev-parse 2>/dev/null
 
 if [[ $? != 0 ]]
 then
-    echo "Not a git repository."
-    exit 1
+  echo "Not a git repository."
+  exit 1
 fi
 
 remote="origin"
 if [ ! -z "$1" ]
 then
-    remote="$1"
+  remote="$1"
 fi
 
 remote_url="remote.${remote}.url"
@@ -23,8 +19,8 @@ remote_url="remote.${remote}.url"
 giturl=$(git config --get $remote_url)
 if [ -z "$giturl" ]
 then
-    echo "$remote_url not set."
-    exit 1
+  echo "$remote_url not set."
+  exit 1
 fi
 
 giturl=${giturl/git\@github\.com\:/https://github.com/}
@@ -32,14 +28,14 @@ giturl=${giturl%\.git}
 
 if [ -z "$2" ]
 then
-    branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
+  branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
 else
-    branch="$2"
+  branch="$2"
 fi
 
 if [ ! -z "$branch" ]
 then
-    giturl="${giturl}/tree/${branch}"
+  giturl="${giturl}/compare/${branch}?expand=1"
 fi
 
 open $giturl
